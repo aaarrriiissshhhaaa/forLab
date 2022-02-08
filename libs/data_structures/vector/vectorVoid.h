5d9002f4f57587b980c96f6653382b7361984306
arisha <pikaploveme@gmail.com>
2022-02-08 20:36:33 +0300

    init vectorVoid

---
+++ libs/data_structures/vector/vectorVoid.h
@@ -0,0 +1,15 @@
+#ifndef COURSE_VECTORVOID_H
+#define COURSE_VECTORVOID_H
+
+#include <limits.h>
+
+typedef struct vectorVoid {
+    void *data;          // указатель на нулевой элемент вектора
+    size_t size;         // размер вектора
+    size_t capacity;     // вместимость вектора
+    size_t baseTypeSize; // размер базового типа:
+} vectorVoid;
+
+
+
+#endif
