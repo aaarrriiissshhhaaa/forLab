0da636b18e4afc5a09f627412560fe83174babf4
arisha <pikaploveme@gmail.com>
2022-02-08 21:17:04 +0300

    append push / pop functions

--- libs/data_structures/vector/vectorVoid.c
+++ libs/data_structures/vector/vectorVoid.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <memory.h>
 
 //возвращает структуру-дескриптор вектор из n значений
 //и размером базового типа baseTypeSize
@@ -20,7 +21,7 @@ vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
         data = NULL;
 
     return (vectorVoid) {data, 0, n, baseTypeSize};
-};
+}
 
 // изменяет количество памяти на newCapacity, выделенное
 // под хранение элементов вектора v
@@ -44,12 +45,12 @@ void reserveV(vectorVoid *v, size_t newCapacity) {
 // под неиспользуемые элементы вектора v
 void shrinkToFitV(vectorVoid *v){
     reserveV(v, v->size);
-};
+}
 
 // удаляет элементы вектора v
 void clearV(vectorVoid *v){
     v->size = 0;
-};
+}
 
 // освобождает память выделенную вектору v
 void deleteVectorV(vectorVoid *v){
@@ -57,4 +58,61 @@ void deleteVectorV(vectorVoid *v){
     v->data = NULL;
     clearV(v);
     v->capacity = 0;
+}
+
+//возвращает "истина" если вектор v
+//является пустым, иначе "ложь"
+bool isEmptyV(vectorVoid *v){
+    return !v->size;
+};
+
+//возвращает "истина" если вектор v
+//является полным, иначе "ложь"
+bool isFullV(vectorVoid *v){
+    return v->size == v->capacity;
+};
+
+//записывает по адресу destination index-ый элемент вектора v.
+void getVectorValueV(vectorVoid *v, size_t index, void *destination){
+    if (index >= v->size) {
+        fprintf(stderr, "IndexError: a[%u] is not exists", index);
+        exit(1);
+    }
+
+    char *source = (char *) v->data + index * v->baseTypeSize;
+    memcpy(destination, source, v->baseTypeSize);
+};
+
+//записывает на index-ый элемент вектора v значение,
+//расположенное по адресу source
+void setVectorValueV(vectorVoid *v, size_t index, void *source){
+    if (index >= v->size) {
+        fprintf(stderr, "IndexError: a[%u] is not exists", index);
+        exit(1);
+    }
+    char *destination = (char *) v->data + index * v->baseTypeSize;
+    memcpy(destination, source, v->baseTypeSize);
+};
+
+// удаляет последний элемент из вектора v
+void popBackV(vectorVoid *v){
+    if (isEmptyV(v)) {
+        fprintf(stderr, "Vector is empty");
+        exit(1);
+    }
+
+    v->size--;
+};
+
+// добавляет элемент x в конец вектора
+// v. Если вектор заполнен, увеличивает
+// количество выделенной ему памяти в 2 раза
+void pushBackV(vectorVoid *v, void *source){
+    if (v->capacity == 0)
+        reserveV(v, 1);
+    else if (isFullV(v))
+        reserveV(v, v->capacity * 2);
+
+    char *destination = (char *) v->data + v->size * v->baseTypeSize;
+    memcpy(destination, source, v->baseTypeSize);
 };
--- libs/data_structures/vector/vectorVoid.h
+++ libs/data_structures/vector/vectorVoid.h
@@ -2,6 +2,7 @@
 #define COURSE_VECTORVOID_H
 
 #include <limits.h>
+#include <stdbool.h>
 
 typedef struct vectorVoid {
     void *data;          // указатель на нулевой элемент вектора
@@ -14,18 +15,41 @@ typedef struct vectorVoid {
 //и размером базового типа baseTypeSize
 vectorVoid createVectorV(size_t n, size_t baseTypeSize);
 
-// изменяет количество памяти на newCapacity, выделенное
-// под хранение элементов вектора v
+//изменяет количество памяти на newCapacity, выделенное
+//под хранение элементов вектора v
 void reserveV(vectorVoid *v, size_t newCapacity);
 
-// освобождает память, выделенную
-// под неиспользуемые элементы вектора v
+//освобождает память, выделенную
+//под неиспользуемые элементы вектора v
 void shrinkToFitV(vectorVoid *v);
 
-// удаляет элементы вектора v
+//удаляет элементы вектора v
 void clearV(vectorVoid *v);
 
-// освобождает память выделенную вектору v
+//освобождает память выделенную вектору v
 void deleteVectorV(vectorVoid *v);
+
+//возвращает "истина" если вектор v
+//является пустым, иначе "ложь"
+bool isEmptyV(vectorVoid *v);
+
+//возвращает "истина" если вектор v
+//является полным, иначе "ложь"
+bool isFullV(vectorVoid *v);
+
+//записывает по адресу destination index-ый элемент вектора v.
+void getVectorValueV(vectorVoid *v, size_t index, void *destination);
+
+//записывает на index-ый элемент вектора v значение,
+//расположенное по адресу source
+void setVectorValueV(vectorVoid *v, size_t index, void *source);
+
+// удаляет последний элемент из вектора v
+void popBackV(vectorVoid *v);
+
+// добавляет элемент x в конец вектора
+// v. Если вектор заполнен, увеличивает
+// количество выделенной ему памяти в 2 раза
+void pushBackV(vectorVoid *v, void *source);
 
 #endif
