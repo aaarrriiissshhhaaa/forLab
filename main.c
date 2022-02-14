a1f1ad6e25ca83db554bfc2c67bf391d19841b41
arisha <pikaploveme@gmail.com>
2022-02-14 20:18:54 +0300

    вторая задача + в библе два доп анекдота

--- main.c
+++ main.c
@@ -7,22 +7,41 @@
 #include "libs/test/testMatrix/testMatrix.h"
 #include "libs/data_structures/matrix/matrix.h"
 
-void swapMaxMinIndex(matrix m){
+
+#define N 4
+void swapRowsMaxMinElement(matrix m){
     position min = getMinValuePos(m);
     position max = getMaxValuePos(m);
 
     swapRowsWithoutVerificationIndex(m, min.rowIndex, max.rowIndex);
 }
 
+int getMax(int *a, int size){
+    int max = a[0];
+    for (int i = 1; i < size; i++)
+        if(a[i] > max)
+            max = a[i];
+    return max;
+}
+
+
+void sortRowsByMinElement(matrix m){
+    insertionSortRowsMatrixByRowCriteria(m, getMax);
+}
+
 int main() {
     //тестов пока нет. Напишу может быть завтра.
-    matrix c = createMatrixFromArray((int[]){4, 6, 7,
-                                             2, 9, 1,
-                                             3, 24, 8,
-                                             11, 5, 0}, 4, 3);
+    int a[] = {1, 2, 3, 4};
 
-    swapMaxMinIndex(c);
+    //printf("%d ", getMax(a, 4));
+
+    matrix c = createMatrixFromArray((int[]){7, 1, 2,
+                                             1, 8, 1,
+                                             3, 2, 3}, 3, 3);
+
+    sortRowsByMinElement(c);
     outputMatrix(c);
 
+    freeMemMatrix(c);
     return 0;
 }
